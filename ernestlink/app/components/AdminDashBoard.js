efa185ab2f40a489993916093ba3c6351be73483
derickddo <derickdanso80@yahoo.com>
2025-05-26 16:51:13 +0000

    superadmin dashboard done

--- ernestlink/app/components/AdminDashBoard.js
+++ ernestlink/app/components/AdminDashBoard.js
@@ -1,14 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import DashboardLayout from "../components/SideBar";
-import BadgeIcon from "@mui/icons-material/Badge";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PeopleIcon from "@mui/icons-material/People";
+import React from "react";
 import { Box, Grid, Paper, Typography, Button } from "@mui/material";
 import ContactPageOutlinedIcon from "@mui/icons-material/ContactPageOutlined";
 import GroupIcon from "@mui/icons-material/Group";
 
-// Components for each menu item
 function AdminDashboard() {
   return (
     <Box
@@ -371,51 +365,5 @@ function AdminDashboard() {
         </Box>
       </Box>
     </Box>
-  );
-}
-
-function Facilitators() {
-  return <div>Manage Facilitators</div>;
-}
-
-function Passports() {
-  return <div>View Passports</div>;
-}
-
-// Menu config
-const adminMenu = [
-  {
-    key: "dashboard",
-    text: "Dashboard",
-    icon: <DashboardIcon />,
-    component: <AdminDashboard />,
-  },
-  {
-    key: "facilitators",
-    text: "Facilitators",
-    icon: <PeopleIcon />,
-    component: <Facilitators />,
-  },
-  {
-    key: "passports",
-    text: "Passports",
-    icon: <BadgeIcon />,
-    component: <Passports />,
-  },
-];
-
-export default function Home() {
-  const [selectedMenu, setSelectedMenu] = useState("dashboard");
-
-  const currentComponent = adminMenu.find((item) => item.key === selectedMenu)
-    ?.component ?? <div>Not Found</div>;
-
-  return (
-    <DashboardLayout
-      menuItems={adminMenu}
-      selectedMenu={selectedMenu}
-      setSelectedMenu={setSelectedMenu}>
-      {currentComponent}
-    </DashboardLayout>
   );
 }
